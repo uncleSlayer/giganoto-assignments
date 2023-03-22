1acf7988067db973d3649b41f03c26b9728cf913
siddhant ota <tothesip@gmail.com>
2023-03-22 20:33:13 +0530

    diagonal sum

--- diagonal_matrix_sum.c
+++ diagonal_matrix_sum.c
@@ -6,7 +6,7 @@
 void l_to_r_diagonal_sum(int matrix[MATRIX_ROW_LEN][MATRIX_COL_LEN])
 {
     int sum = 0;
-    for (int i = 0, j = 3; i < MATRIX_ROW_LEN; i++, j--)
+    for (int i = 0, j = 2; i < MATRIX_ROW_LEN; i++, j--)
     {
         sum = sum + matrix[i][j];
     }
